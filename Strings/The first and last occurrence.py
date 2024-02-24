e3fa8eb7d02615ac86164e98b7987651a585a7d2
mitul002 <mitul15-4617@diu.edu.bd>
2024-02-25 00:29:13 +0600

    snakify

--- Strings/The first and last occurrence.py
+++ Strings/The first and last occurrence.py
@@ -4,4 +4,13 @@ c=(word.count("f"))
 if c==1:
     print(word.index("f"))
 elif c>1:
-    print(word.index("f"),word.count("f"))
+    print(word.index("f"),word.rindex("f"))
+
+
+'''word=str(input())
+
+c=(word.count("f"))
+if c==1:
+    print(word.index("f"))
+elif c>1:
+    print(word.index("f"),word.count("f"))'''
