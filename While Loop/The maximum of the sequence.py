fc4fd2494eaf714294c9f575f5d63fc774ec955b
mitul002 <mitul15-4617@diu.edu.bd>
2024-02-28 20:18:15 +0600

    snakify

--- While Loop/The maximum of the sequence.py
+++ While Loop/The maximum of the sequence.py
@@ -1,12 +1,8 @@
-max = 0
-pos = 0
+max=0
 while True:
-    a = int(input())
-    pos += 1
-    if a > max:
-        max = a
-        max_pos = pos
-
-    if a == 0:
+    a=int(input())
+    if a>max:
+        max=a
+    if a==0:
         break
-print(max_pos)
+print(max)
